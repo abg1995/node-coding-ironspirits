e0fe3ddb2d971c46f3a98ff19f402220f157bb72
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-20 12:44:41 +0200

    handlebars, liosts and nav bar change, products

--- app.js
+++ app.js
@@ -1,19 +1,77 @@
 const express = require('express');
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
 app.use(express.static('public'));
 
 
+
+
+/* Routes */
+
+app.get("/", (req, res, next)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+
 app.get("/about", (req, res, next) => {
-
-   res.sendFile(__dirname + '/views/about.html');
-
+    res.sendFile(__dirname + '/views/about.html');
 });
 
 
 app.get("/contact", (req, res, next) => {
-    res.sendFile(__dirname + '/views/contact.html')
+    res.sendFile(__dirname + '/views/contact.html');
 });
+
+
+
+
+app.get("/limoncello", (req, res, next) => {
+
+    // res.render("view", info);
+
+    const data = {
+        title: "Limoncello",
+        price: 20,
+        imageFile: "limoncello.png",
+        stores: ["Online","Madrid", "Amsterdam","Paris"]
+
+    }
+
+    res.render("product", data);
+});
+
+app.get("/whiskey", (req, res, next) => {
+
+
+    const data ={
+        title: "whiskey",
+        price: 105,
+        imageFile: "suntory-yamazaki.jpeg",
+        stores: ["Online","Madrid", "Amsterdam","Paris"]
+    }
+
+    res.render("product",data);
+});
+
+app.get("/tequila", (req, res, next) => {
+
+    const data = {
+        title: "Tequila",
+        price: 35,
+        imageFile: "donjulio.jpeg",
+        stores: ["Online","Madrid", "Amsterdam","Paris"]
+    }
+
+        res.render("product", data);
+});
+
+
+
+
+
 
 
 app.listen(3001, () => {
