6aec67b52bbb451bfb3cee45628fdcf888cd0045
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-22 11:57:05 +0200

    use route params: create generic rout for all products

--- app.js
+++ app.js
@@ -1,73 +1,46 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Product = require('./models/product.model');
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model");
 const app = express();
-
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 mongoose
-    .connect('mongodb://localhost/ironborn-ecommerce')
-    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-    .catch(err => console.error('Error connecting to mongo', err));
+  .connect("mongodb://localhost/ironborn-ecommerce")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 /* Routes */
 
-app.get("/", (req, res, next)=>{
-    res.render("home");
+app.get("/", (req, res, next) => {
+  res.render("home");
 });
-
 
 app.get("/about", (req, res, next) => {
-    res.render("about");
+  res.render("about");
 });
 
-
 app.get("/contact", (req, res, next) => {
-    res.render("contact");
+  res.render("contact");
 });
 
 app.get("/products", (req, res, next) => {
-    res.render("products");
+  res.render("products");
 });
 
-
-
-app.get("/limoncello", (req, res, next) => {
-    Product.findOne({title:'Limoncello'})
+app.get("/products/:productId", (req, res, next) => {
+  Product.findById(req.params.productId)
     .then((productDetails) => {
-        res.render("product",productDetails);
+      res.render("product", productDetails);
     })
-    .catch( error => console.log(error))
+    .catch((error) => console.log("error getting prod from db", error));
 });
-
-app.get("/whiskey", (req, res, next) => {
-    Product.findOne({title:'Single Malt Whisky Yamakazi'})
-    .then((productDetails) => {
-        res.render("product",productDetails);
-    })
-    .catch( error => console.log(error))
-});
-
-app.get("/tequila", (req, res, next) => {
-    Product.findOne({title:'Tequila Don Julio'})
-    .then((productDetails) => {
-        res.render("product",productDetails);
-    })
-    .catch( error => console.log(error))});
-
-
-
-
-
-
 
 app.listen(3001, () => {
-    console.log("server listening to requests...")
+  console.log("server listening to requests...");
 });
-
-
