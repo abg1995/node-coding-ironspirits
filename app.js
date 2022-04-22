87fcb412c0d9a14239dbe4a68a0b42a18406e331
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-22 16:45:00 +0200

    POST & functionality to create new products

--- app.js
+++ app.js
@@ -1,55 +1,92 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Product = require("./models/product.model");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Product = require('./models/Product.model');
 const app = express();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+/* Connect to DB */
 mongoose
-  .connect("mongodb://localhost/ironborn-ecommerce")
-  .then((x) =>
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  )
-  .catch((err) => console.error("Error connecting to mongo", err));
+    .connect('mongodb://localhost/ironborn-ecommerce')
+    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch(err => console.error('Error connecting to mongo', err));
+
+
 
 /* Routes */
 
 app.get("/", (req, res, next) => {
-  res.render("home");
+    res.render("home");
 });
+
 
 app.get("/about", (req, res, next) => {
-  res.render("about");
+    res.render("about");
 });
 
+
 app.get("/contact", (req, res, next) => {
-  res.render("contact");
+    res.render("contact");
 });
+
 
 app.get("/products", (req, res, next) => {
 
+    let filter;
     const max = req.query.maxPrice;
+    
+    if(max === undefined){
+        filter = {};
+    } else {
+        filter = {price: {$lte: max} };
+    }
 
-    Product.find({price: {$lte: max}})
-    .then( productsArr => {
-        res.render('productList', {products: productsArr});
-    })
-    .catch( (error) => {console.log('error getting products from DB',error)})
-
+    Product.find(filter)
+        .then( productsArr => {
+            res.render("productList", {products: productsArr} );
+        })
+        .catch( error => console.log("error getting products from DB", error) );
 })
 
+
 app.get("/products/:productId", (req, res, next) => {
-  Product.findById(req.params.productId)
-    .then((productDetails) => {
-      res.render("product", productDetails);
-    })
-    .catch((error) => console.log("error getting prod from db", error));
-});
+    Product.findById(req.params.productId)
+        .then(productDetails => {
+            res.render("product", productDetails);
+        })
+        .catch(error => console.log("error getting product from DB", error));
+})
+
+
+app.post("/new", (req,res,next) => {
+    console.log("creating new product")
+    console.log(req.body)
+
+        const newProduct = {
+                title: req.body.title,
+                price: req.body.price
+              };
+
+
+    Product.create(newProduct)
+              .then( newProduct => {
+                  res.redirect("/products",)
+              })
+              .catch(error => {console.log("error creating new product", error)
+                    res.redirect("/")})
+})
+
+
 
 
 app.listen(3001, () => {
-  console.log("server listening to requests...");
+    console.log("server listening to requests...")
 });
+
+
